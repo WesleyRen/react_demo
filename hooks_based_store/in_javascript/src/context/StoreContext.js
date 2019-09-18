81eca33a33f90d308af7eb610936a80853672ec7
Wesley Ren <wren@walmartlabs.com>
2019-09-17 17:19:38 -0700

    added logger for store action and state

--- hooks_based_store/in_javascript/src/context/StoreContext.js
+++ hooks_based_store/in_javascript/src/context/StoreContext.js
@@ -1,13 +1,34 @@
-import React, { createContext, useEffect, useReducer } from 'react';
+import React, { createContext, useEffect, useRef, useReducer } from 'react';
 import { initialState, reducer } from './reducers';
 import useActions from './actions';
 
 const StoreContext = createContext(initialState);
+
+function useLogger([state, dispatch]) {
+    const actionRef = useRef(null);
+
+    const newDispatchRef = useRef(action => {
+        actionRef.current = action;
+        dispatch(action);
+    });
+
+    useEffect(() => {
+        const action = actionRef.current;
+
+        if (action) {
+            console.group('Dispatch');
+            console.log('Action: ', action);
+            console.log('State: ', state);
+            console.groupEnd();
+        }
+    }, [state]);
+
+    return [state, newDispatchRef.current];
+}
+
 const StoreProvider = ({ children }) => {
-    const [ state, dispatch ] = useReducer(reducer, initialState);
+    const [ state, dispatch ] = useLogger(useReducer(reducer, initialState));
     const actions = useActions(state, dispatch);
-    // Log new state
-    useEffect(() => { console.log({ newState: state }); }, [ state ]);
     return (
         <StoreContext.Provider value={{ state, dispatch, actions }}>
             {children}
