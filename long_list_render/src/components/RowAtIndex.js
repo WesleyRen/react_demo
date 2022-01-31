9c354982be31a079f80be3458f20bfc3492886b2
Wesley Ren <wren@walmartlabs.com>
2022-01-31 12:06:05 -0800

    fix: make scroll indicator follow the mouse when drag

--- long_list_render/src/components/RowAtIndex.js
+++ long_list_render/src/components/RowAtIndex.js
@@ -2,15 +2,12 @@ import React from "react";
 
 export const rowHeight = 30;
 
-const paddingTop = 5;
-
 export function RowAtIndex(index) {
     return (
         <div
             style={{
-                height: rowHeight - (2 * paddingTop),
-                color: toColor(index),
-                padding: paddingTop + "px 10px",
+                height: rowHeight,
+                color: computeHSLRainbowColor(index, 100),
                 fontSize: 24
             }}
         >
@@ -19,19 +16,6 @@ export function RowAtIndex(index) {
     );
 }
 
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function toColor(num) {
-    const d1 = num % 10;
-    const d2 = Math.floor(num / 10) % 10;
-    const d3 = Math.floor(num / 100) % 10;
-    let result = "rgb(" + [d1 * 25, d2 * 25, d3 * 25].join(",") + ")";
-    return result;
+function computeHSLRainbowColor(n, period) {
+  return `hsl(${Math.round(n / period * 360)},100%,${(n % period) / period * 100}%)`;
 }
