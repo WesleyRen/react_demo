d23adcd390782e3ca1b8dfd9931758a4085af236
Wesley Ren <wesleyren@yahoo.com>
2023-03-25 22:19:27 -0700

    migrate to typescript, from scratch, in a new dir

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -6,7 +6,14 @@ import {logger} from "redux-logger";
 import thunk from "redux-thunk";
 import App from "./App";
 import {combo} from "./reducers/MyReducer";
+import reportWebVitals from './reportWebVitals';
 
 const store = createStore(combineReducers(combo), applyMiddleware(thunk, logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
