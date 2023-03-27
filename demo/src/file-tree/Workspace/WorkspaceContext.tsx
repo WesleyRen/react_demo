0d5a813843648b38649ce8c6ee169b6e26505feb
Wesley Ren <wesleyren@yahoo.com>
2023-03-26 22:29:20 -0700

    add starter code for file tree

---
+++ demo/src/file-tree/Workspace/WorkspaceContext.tsx
@@ -0,0 +1,41 @@
+import React, { createContext, ReactNode, useContext, useMemo, useState } from 'react'
+
+export type File = {
+  path: string
+  contents: string
+}
+
+export const workspaceContext = createContext<{
+  activeFile: File | null,
+  activateFile: React.Dispatch<React.SetStateAction<string | null>>,
+  files: File[]
+}>({
+  activeFile: null,
+  activateFile: () => {},
+  files: [],
+})
+
+export const WorkspaceProvider: React.FC<{ files: File[], children: ReactNode }> = ({ files, children }) => {
+  const [activeFilePath, setActiveFilePath] = useState<string|null>(null)
+
+  const activeFile = useMemo(() => {
+    const foundFile = files.find((f) => f.path === activeFilePath)
+    return foundFile || files[0]
+  }, [activeFilePath])
+
+  const ctxVal: {
+    activeFile: File | null,
+    activateFile: React.Dispatch<React.SetStateAction<string | null>>,
+    files: File[]
+  } = {
+    activeFile,
+    activateFile: setActiveFilePath,
+    files,
+  }
+
+  return <workspaceContext.Provider value={ctxVal}>{children}</workspaceContext.Provider>
+}
+
+export function useWorkspaceContext() {
+  return useContext(workspaceContext)
+}
