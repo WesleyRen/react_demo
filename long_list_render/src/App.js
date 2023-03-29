c6a434da2c3d8d3b6a195875a02a77d5f89b7a48
Wesley Ren <wesleyren@yahoo.com>
2023-03-28 22:08:40 -0700

    remove absolete files

--- long_list_render/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { LongList } from "./components/LongList";
-import { RowAtIndex, rowHeight } from "./components/RowAtIndex";
-
-export default function App() {
-    // Interesting, it seems browser (Chrome) has a scroll top limit around 30,503,022.
-    // That causes anything > 1016801 here not able to show.
-    return <LongList {...{numRows: 1016801, rowHeight, renderRow: RowAtIndex}} />
-}
