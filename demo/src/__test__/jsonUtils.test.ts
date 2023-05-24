003c6e769fb0118dc90a2dbc8f7f4ad07e098775
Wesley Ren <wesleyren@yahoo.com>
2023-05-23 22:02:11 -0700

    add json flattener, by chatGPT

--- demo/src/__test__/jsonUtils.test.ts
+++ demo/src/__test__/jsonUtils.test.ts
@@ -1,24 +1,31 @@
-import { flattenJson } from "../code-snippets/jsonUtils";
+import { flattenJson, flattenJson2 } from "../code-snippets/jsonUtils";
+
+const json = {
+  "name": "John",
+  "age": 30,
+  "cars": {
+      "car1": "Ford",
+      "car2": "BMW",
+      "car3": "Fiat"
+  }
+}
+const expected = {
+  name: 'John',
+  age: 30,
+  'cars.car1': 'Ford',
+  'cars.car2': 'BMW',
+  'cars.car3': 'Fiat'
+};
+
 
 describe(flattenJson.name, () => {
   test('flattens JSON object', () => {
-    const json = {
-        "name": "John",
-        "age": 30,
-        "cars": {
-            "car1": "Ford",
-            "car2": "BMW",
-            "car3": "Fiat"
-        }
-    }
-    const expected = {
-      name: 'John',
-      age: 30,
-      'cars.car1': 'Ford',
-      'cars.car2': 'BMW',
-      'cars.car3': 'Fiat'
-    };
-    
     expect(expected).toEqual(flattenJson(json));
+  })
+})
+
+describe(flattenJson2.name, () => {
+  test('flattens JSON object', () => {    
+    expect(expected).toEqual(flattenJson2(json));
   })
 })
